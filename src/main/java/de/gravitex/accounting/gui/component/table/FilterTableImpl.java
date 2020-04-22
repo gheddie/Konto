49b5253fe57d4fda7c1f250b4b64d7d56203c6d0
Sts <Sts@view-localhost>
2020-04-22 11:09:07 +0200

    123

--- src/main/java/de/gravitex/accounting/gui/component/table/FilterTableImpl.java
+++ src/main/java/de/gravitex/accounting/gui/component/table/FilterTableImpl.java
@@ -5,4 +5,14 @@ import javax.swing.JTable;
 public class FilterTableImpl extends JTable {
 
 	private static final long serialVersionUID = 9146563169960538428L;
+	
+	public FilterTableImpl() {
+		super();
+		getTableHeader().setReorderingAllowed(false);
+	}
+	
+	@Override
+	public boolean isCellEditable(int row, int column) {
+		return false;
+	}
 }
